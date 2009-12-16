76fc168362834aa40c7915dd4c800ddc7e8cc4f9
ezindy <ezindy@e171149b-3840-0410-83e7-8b9f36f6a97e>
2009-12-16 23:57:55 +0000

    Added inspection tool to xrcMixin.py and updated gui.py

--- trunk/xrcmixin/xrcMixin.py
+++ trunk/xrcmixin/xrcMixin.py
@@ -28,33 +28,60 @@ Released under the MIT licence.
 __author__ = "Egor Zindy <ezindy@gmail.com>"
 __copyright__ = "Copyright (c) 2009 Egor Zindy"
 __license__ = "MIT"
-__version__ = "1.0"
-__date= "2009-12-09"
+__version__ = "1.0.1"
+__date= "2009-12-16"
 __status__ = "Production"
 
 import wx
 import wx.lib.newevent
+
 DebugEvent, EVT_DEBUG = wx.lib.newevent.NewEvent()
 
 class xrcMixin():
-    def __init__(self,debug=False):
-        self.debug = debug
+    def __init__(self,debug=False,InspectionTool=None):
 
         self.dict = {}
         for ctrl in self.GetChildren():
             name = ctrl.GetName()
-
             if name != "-1":
-                if debug:
-                    ctrl.SetToolTip(wx.ToolTip(name))
-                    if name in self.dict.keys():
-                        print "Multiple instance of: %s" % name
-
                 setattr(self,name,ctrl)
                 self.dict[name]=ctrl
 
-        if debug:
-            self.Bind(wx.EVT_LEFT_DCLICK,self.OnDebug)
+        self.InspectionTool = InspectionTool
+        self.SetDebug(debug)
+
+    def SetDebug(self,debug):
+        """Set the debug level.
+
+        0 disables debugging output, 1 enables it (on panel double-click).
+        """
+
+        self.debug = debug
+
+        if debug == 0:
+            return
+
+        #dealing with multiple names
+        dict = {}
+        is_multiple = 0
+        s = "Multiple names:\n"
+        for ctrl in self.GetChildren():
+            name = ctrl.GetName()
+
+            if name != "-1":
+                ctrl.SetToolTip(wx.ToolTip(name))
+
+                if name in dict.keys():
+                    s+="\n%s" % name
+                    is_multiple = 1
+
+                dict[name]=1
+
+        if is_multiple == 1:
+            wx.MessageBox(s, '%s debug' % self.__class__.__name__)
+
+        #binding a custom debug event
+        self.Bind(wx.EVT_LEFT_DCLICK,self.OnDebug)
 
     def GetValues(self):
         """Values from panel controls
@@ -73,15 +100,16 @@ class xrcMixin():
                 dict[key]=ctrl.GetSelection()
             elif 'GetCurrentSelection' in methods:
                 dict[key]=ctrl.GetCurrentSelection()
-
         return dict
 
     def OnDebug(self,evt):
-        s = "Control values:\n\n"
-        for key,value in self.GetValues().items():
-            s+="%s: %s\n" % (key,str(value))
-
-        wx.MessageBox(s, 'Debug')
+        if self.InspectionTool is None:
+            s = "Control values:\n"
+            for key,value in self.GetValues().items():
+                s+="\n%s: %s" % (key,str(value))
+            wx.MessageBox(s, '%s debug' % self.__class__.__name__)
+        else:
+            self.InspectionTool.Show(self)
 
         debug_event = DebugEvent( GetValues = self.GetValues)
         wx.PostEvent(self, debug_event)
